c54588b62a877f5dbe33ca8dfa81caff2921e48d
Brandon Farmer <bthesorceror@gmail.com>
2013-01-01 23:26:13 -0500

    Fix global variable definition

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function Rivulet(path) {
 Rivulet.prototype.middleware = function() {
   var self = this;
   return function(req, res, next) {
-    if (match = req.url.match(self.regex)) {
+    var match = req.url.match(self.regex)
+    if (match) {
       var path = match[1];
       res.writeHead(200, { 'Content-Type': 'text/event-stream' });
       self.emitter.on(path, function(data) {
