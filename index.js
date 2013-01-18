ae6059ce16d8a6e74bc03fc2ef51b7cdd1db3a92
Brandon Farmer <bthesorceror@gmail.com>
2013-01-17 23:21:31 -0500

    Update interface

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 var EventEmitter = require('events').EventEmitter,
     fs = require('fs');
 
-function superProxy(func, context) {
+var event_stream_header = {
+                            'Content-Type':  'text/event-stream',
+                            'Cache-Control': 'no-cache',
+                            'Connection':    'keep-alive'
+                          }
+
+function superProxyMe(func, context) {
   return function() {
     func.apply(context, arguments);
   }
 }
 
-function Rivulet(hub, path) {
-  this.path = path || 'rivulets';
-  this.emitter = new EventEmitter();
-  this.regex = new RegExp('/' + this.path + '/(.*)');
+function generateListener(res) {
+  return function(data, event_type) {
+    var json = JSON.stringify(data);
+    if (event_type) {
+      res.write("event: " + event_type + "\n");
+    }
+    res.write("data: " + json + "\n\n");
+  };
+}
+
+function Rivulet(hub, path, options) {
+  options = options || {};
+  this.path        = path || 'rivulets';
+  this.emitter     = new EventEmitter();
+  this.regex       = new RegExp('/' + this.path + '/(.*)');
   this.static_path = '/' + this.path + '/event-source.js';
-  hub && hub.on(this.path, superProxy(this.send, this));
+  this.polyfill    = options['polyfill'] || __dirname + '/static/event-source.js';
+  hub && hub.on(this.path, superProxyMe(this.send, this));
 }
 
 Rivulet.prototype.middleware = function() {
   var self = this;
   return function(req, res, next) {
-    var match = req.url.match(self.regex)
+    var match = req.url.match(self.regex);
     if (req.url == self.static_path) {
-      res.writeHead(200, { 'Content-Type': 'text/application' });
-      fs.
-        createReadStream(__dirname + '/static/event-source.js').
-        pipe(res);
+      res.writeHead(200, { 'Content-Type': 'application/javascript' });
+      fs.createReadStream(self.polyfill).pipe(res);
     } else if (match) {
-      var path = match[1];
+      var path     = match[1],
+          listener = generateListener(res);
+
       req.socket.setTimeout(Infinity);
-      res.writeHead(200,
-                    { 'Content-Type':  'text/event-stream' ,
-                      'Cache-Control': 'no-cache',
-                      'Connection':    'keep-alive' });
-      var listener = function(data, event_type) {
-        var json = JSON.stringify(data);
-        if (event_type) {
-          res.write("event: " + event_type + "\n");
-        }
-        res.write("data: " + json + "\n\n");
-      };
+      res.writeHead(200, event_stream_header);
 
       self.emitter.on(path, listener);
 
