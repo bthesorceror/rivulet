768910da5edb7fde5b78609d75810ff6dbb2aada
Brandon Farmer <bthesorceror@gmail.com>
2013-01-23 13:19:40 -0500

    Remove ability to attach to event emitter

--- rivulet.js
+++ rivulet.js
@@ -1,6 +1,7 @@
 var EventEmitter = require('events').EventEmitter,
     fs           = require('fs'),
     path         = require('path'),
+    hash         = require('hashish'),
     helpers      = require('./lib/helpers');
 
 var event_stream_header = {
@@ -9,18 +10,19 @@ var event_stream_header = {
   'Connection':    'keep-alive'
 }
 
-var default_polyfill = path.join(__dirname, '/static/event-source.js');
+var default_options = {
+  path: 'rivulets',
+  polyfill: path.join(__dirname, '/static/event-source.js'),
+}
 
 
 function Rivulet(options) {
-  options = options || {};
-  this.path        = options['path'] || 'rivulets';
+  options = hash.merge(default_options, options);
+  this.path        = options['path'];
   this.emitter     = new EventEmitter();
   this.regex       = new RegExp('/' + this.path + '/(.*)');
   this.static_path = '/' + this.path + '/event-source.js';
-  this.polyfill    = options['polyfill'] || default_polyfill;
-  this.hub         = options['hub'] || null;
-  this.hub && this.hub.on(this.path, helpers.superProxyMe(this.send, this));
+  this.polyfill    = options['polyfill'];
 }
 
 Rivulet.prototype.middleware = function() {
